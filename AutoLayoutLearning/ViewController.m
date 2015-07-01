4d284952f094a8a350306fd46b3057e2fd51287f
angelolobo <ange101lobo@gmail.com>
2015-07-01 16:07:03 +0530

    Update ViewController.m

--- AutoLayoutLearning/ViewController.m
+++ AutoLayoutLearning/ViewController.m
@@ -27,6 +27,8 @@
 }
 
 -(void)setupViews{
+
+    //These views have to be added as properties in actual code
     UIView *redView = [UIView new];
     redView.translatesAutoresizingMaskIntoConstraints = NO;
     redView.backgroundColor = [UIColor colorWithRed:0.95 green:0.47 blue:0.48 alpha:1.0];
